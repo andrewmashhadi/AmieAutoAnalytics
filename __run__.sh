2b10a8801d27cf036d29dd418ebe961c9df22dea
EC2 Default User <ec2-user@ip-172-31-27-135.us-east-2.compute.internal>
2024-12-30 07:48:33 +0000

    began test on AWS EC2 instance

--- __run__.sh
+++ __run__.sh
@@ -1,8 +1,7 @@
-#!/bin/zsh
+#!/bin/bash
 
-# Load Conda for Zsh (adjust the path to match your Conda installation)
-source $HOME/.zshrc
-source $HOME/opt/anaconda3/etc/profile.d/conda.sh 
+source /home/ec2-user/.bashrc # for env variables
+source /home/ec2-user/miniconda3/etc/profile.d/conda.sh  
 
 # Activate the Conda environment
 conda activate aaa-env
@@ -14,7 +13,7 @@ echo "**************************************************************************
 echo " "
 
 # Run the Python script
-python $HOME/LiveProjects/AmieAutoAnalytics/main.py  # Replace with the correct path to your Python script
+python /home/ec2-user/projects/AmieAutoAnalytics/main.py  # Replace with the correct path to your Python script
 
 # Deactivate Conda environment
 conda deactivate
